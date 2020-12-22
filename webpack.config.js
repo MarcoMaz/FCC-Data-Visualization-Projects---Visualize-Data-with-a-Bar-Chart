03248875635ecb3c455e8851d8750b0423781269
Marco Mazzai <mazzai.marco@gmail.com>
2020-12-22 21:33:04 +0100

    fifth commit

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + '/src/public/index.html',
+      template: __dirname + '/index.html',
       inject: 'body'
     }),
     new ExtractTextPlugin('main.css')
